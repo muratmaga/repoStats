0556859fa39b58a6dac5be2af57d0470c488c5aa
Murat Maga <muratmaga@outlook.com>
2025-09-14 22:08:34 -0700

    correct cron logic

--- execute.sh
+++ execute.sh
@@ -5,6 +5,6 @@ WEEK_NUM=$(date +%W)
 
 # Check if the week number is even or odd (adjust as needed for your desired starting Sunday)
 # For odd numbered weeks:
-if (( WEEK_NUM % 2 != 0 )); then
+if (( WEEK_NUM % 2 == 0 )); then
     /home/maga/repoStats/getStats.sh
 fi
